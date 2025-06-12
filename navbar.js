ebbc4d1d187d81fd1a069733a019e247103446a2
MysteriousWolf19 <shenaytopper28@gmail.com>
2025-06-12 15:47:42 +0200

    fixnav

--- navbar.js
+++ navbar.js
@@ -4,18 +4,4 @@ fetch('navbar.html')
   .then(data => {
     document.getElementById('navbar').innerHTML = data;
   })
-  .then(() => {
-    const button = document.querySelector('.menu-button');
-    const menu = document.querySelector('.dropdown-menu');
-
-    button.addEventListener('click', (e) => {
-      e.stopPropagation();
-      menu.classList.toggle('show');
-    });
-
-    // Close menu on outside click
-    document.addEventListener('click', () => {
-      menu.classList.remove('show');
-    });
-  })
   .catch(err => console.error('Error loading navbar:', err));
